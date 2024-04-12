091e3b34c453ed5d0329443f416f3a2d2618322b
makerzy <botassammzy@gmail.com>
2024-04-12 08:26:20 -0700

    removed comments

--- main.py
+++ main.py
@@ -17,22 +17,17 @@ def remove_page(pdf_file, page_number, output_full_path):
 def add_page(pdf_file, page_file, output_full_path):
     writer = PyPDF2.PdfFileWriter()
     
-    # Open the input PDF file
     with open(pdf_file, 'rb') as file:
         reader = PyPDF2.PdfFileReader(file)
         
-        # Add pages from input PDF
         for i in range(reader.numPages):
             writer.addPage(reader.getPage(i))
 
-        # Open the page PDF file
         with open(page_file, 'rb') as page:
             page_reader = PyPDF2.PdfFileReader(page)
             
-            # Add the first page from the page PDF
             writer.addPage(page_reader.getPage(0))
             
-            # Write the combined PDF to a new file
             with open(output_full_path, 'wb') as output_file:
                 writer.write(output_file)
 
